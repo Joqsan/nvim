c2995eacc2c14033ff5c98b452e37e7255cf1cd5
Joqsan Azocar <joqsan.a@gmail.com>
2024-01-28 22:34:52 +0300

    add comments to each line.
    add pyright
    
    add snippet engine
    
    resort cmp sources

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,3 +1,5 @@
+-- lsp-zero first adds data to an option called capabilities in lspconfig's defaults. 
+-- This new data comes from cmp-nvim-lsp. It tells the language server what features nvim-cmp adds to the editor.
 local lsp_zero = require('lsp-zero')
 
 -- keymap happening only on open buffer with an LSP associated
@@ -15,7 +17,7 @@ lsp_zero.on_attach(function(client, bufnr)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
 
-  -- for stuff like automatic import of modules
+  -- for stuff like automatic sorting  of imports
   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
 
   -- see where the variable under the cursor is being used
@@ -28,35 +30,54 @@ lsp_zero.on_attach(function(client, bufnr)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
-require('mason').setup({})
+-- Make sure to set up `mason` and `mason-lspconfig.nvim` before 
+-- setting up servers via `lspconfig`.
+require("mason").setup()
 require('mason-lspconfig').setup({
-  ensure_installed = {"lua_ls"},
+  ensure_installed = {"lua_ls", "pyright"},
   handlers = {
-    lsp_zero.default_setup,
-    lua_ls = function()
+    -- The first entry (without a key) will be the default handler -- and will be called
+    -- for each installed server that doesn't have a dedicated handler.
+    lsp_zero.default_setup, 
+    -- A dedicated handler for specific servers. 
+    lua_ls = function()  -- Setting up Lua for neovim (https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/neovim-lua-ls.md#fixed-config)
       local lua_opts = lsp_zero.nvim_lua_ls()
       require('lspconfig').lua_ls.setup(lua_opts)
     end,
   }
 })
 
--- configure completions
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 
 cmp.setup({
-  sources = {
-    {name = 'path'},
-    {name = 'nvim_lsp'},
-    {name = 'nvim_lua'},
-    {name = 'luasnip', keyword_length = 2},
-    {name = 'buffer', keyword_length = 3},
-  },
-  formatting = lsp_zero.cmp_format(),
+  -- Custom mappings.
+  -- -- It's important to use nvim-cmp's preset so you don't lose the default keybindings.
   mapping = cmp.mapping.preset.insert({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete(),
   }),
+  -- snippet: how nvim-cmp interacts with a particular snippet engine.
+  snippet = {
+    expand = function(args)
+      -- pick the snippet plugin you have
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+    end,
+  },
+  completion = {
+    completeopt = 'menu,menuone,noinsert',
+  },
+  -- The purpose of a source is to extract data and then pass it to nvim-cmp.
+  -- The order of the sources determines their order in the completion results.
+  sources = {
+    {name = 'nvim_lua'},
+    {name = 'nvim_lsp'},
+    {name = 'path'},
+    {name = 'luasnip'},
+    {name = 'buffer', keyword_length = 5},
+  },
+  formatting = lsp_zero.cmp_format(),
+  
 })
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,16 +9,32 @@ return {
         
 
         -- Autocompletion
-        {'hrsh7th/cmp-nvim-lsp'},
-        {'hrsh7th/nvim-cmp'},
+        {'hrsh7th/nvim-cmp'},  -- core autocompletion engine
 
+        -- -- cmp-nvim-lsp: lsp-powered autocompletion. For example,
+        -- -- auto-import on complete, moving between snippets
+        {'hrsh7th/cmp-nvim-lsp'}, 
+        -- -- cmp-buffer: completes words from the current byffer you're in.
+        -- -- Allow nvim-cmp to show completions even when we don't have a 
+        -- -- language server active in the current buffer.
         {'hrsh7th/cmp-buffer'},
+
+        -- -- cmp-path: it helps complete files, so it will detect when it looks
+        -- -- like you're typing a file.
         {'hrsh7th/cmp-path'},
-        {'saadparwaiz1/cmp_luasnip'},
+
+        -- -- cmp-nvim-lua: for writing Lua in Neovim. like getting completion for
+        -- -- Neovim API functions
         {'hrsh7th/cmp-nvim-lua'},
 
         -- Snippets
         {'L3MON4D3/LuaSnip'},
+        -- -- cmp_luasnip: to be used with LuaSnip. Load custom snippets into the
+        -- -- completion menu 
+        {'saadparwaiz1/cmp_luasnip'},
+        
+        
+        -- -- friendly-snippets: so that we don't write our own snippets.
         {'rafamadriz/friendly-snippets'},
     }
 }
