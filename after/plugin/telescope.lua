5c405c64bde6ded936534bdbef5235b8bb520492
Joqsan Azocar <joqsan.a@gmail.com>
2024-01-28 22:38:02 +0300

    add lsp_reference to Telescope remap

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,6 +2,9 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})  -- pf: project files
 vim.keymap.set('n', '<leader>pg', builtin.git_files, {})  -- pg: project git
 vim.keymap.set('n', '<leader>gs', builtin.git_status, {})  -- pg: project git
+
+-- lr: show all the places in the project where the word under the cursor is referenced
+vim.keymap.set('n', '<leader>lr', builtin.lsp_references, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)  -- ps: project search 
