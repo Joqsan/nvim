83aa2cc3fc1c03c63142fd30d2afa6e1a88179b8
Joqsan Azocar <joqsan.a@gmail.com>
2024-02-03 17:40:34 +0300

    Add bash parse to fix parsing problem

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,7 +1,11 @@
 local configs = require("nvim-treesitter.configs")
 
 configs.setup({
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python" },
+    ensure_installed = {
+        "c", "lua", "vim", "vimdoc", "query", 
+        "python",
+        "bash",
+    },
     sync_install = false,
     highlight = { enable = true, additional_vim_regex_highlighting = false},
     indent = { enable = true },  
