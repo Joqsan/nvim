8d0786503a6adfcc76c2f40bdc20779ec9c56df4
Joqsan Azocar <joqsan.a@gmail.com>
2024-02-13 00:33:59 +0300

    refactor telescope remaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,14 +1,15 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', builtin.git_status, {})
-vim.keymap.set('n', '<leader>bf', builtin.current_buffer_fuzzy_find, {})
+vim.keymap.set('n', '<leader>tf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>tg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ts', builtin.git_status, {})
+vim.keymap.set('n', '<leader>tb', builtin.current_buffer_fuzzy_find, {})
 -- lr: show all the places in the project where the word under the cursor is referenced
-vim.keymap.set('n', '<leader>lr', builtin.lsp_references, {})
-vim.keymap.set('n', '<leader>gs', function()
+vim.keymap.set('n', '<leader>tr', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)  -- ps: project search 
 
+vim.keymap.set('n', '<leader>tl', builtin.lsp_references, {})
 vim.keymap.set("n", "<leader>td", builtin.diagnostics, opts)
-
 vim.keymap.set('n', '<leader>th', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>to', builtin.lsp_outgoing_calls, {})
+vim.keymap.set('n', '<leader>ti', builtin.lsp_incoming_calls, {})
