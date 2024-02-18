fafa42faa138599ffca9951804d62dc8ed572506
Joqsan Azocar <joqsan.a@gmail.com>
2024-02-19 00:03:12 +0300

    new remap for resizing and moving between windows

--- lua/my-dir/remap.lua
+++ lua/my-dir/remap.lua
@@ -2,14 +2,14 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>')
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {desc = "Go to Netrw"})
 
 -- select lines and move them up and down, preserving indenting
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {desc = "Move selected lines down, preserving indenting"})
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {desc = "Move selected lines up, preserving indenting"})
 
 -- Join lines preserving the current cursor position
-vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "J", "mzJ`z", {desc = "Join lines preserving the current cursor position"})
 
 -- Go down and up, keeping the cursor in the middle
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -36,7 +36,8 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+-- turn off because of overlapping
+-- vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
@@ -60,8 +61,8 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- git diff
-vim.keymap.set("n", "<leader>-", "<cmd>diffg LO<CR>", { silent = true })
-vim.keymap.set("n", "<leader>=", "<cmd>diffg RE<CR>", { silent = true })
+vim.keymap.set("n", "<leader>[", "<cmd>diffg LO<CR>", { silent = true })
+vim.keymap.set("n", "<leader>]", "<cmd>diffg RE<CR>", { silent = true })
 vim.keymap.set("n", "<leader>od", function()
     if vim.opt.diff:get() then
         vim.cmd("diffoff")
@@ -69,3 +70,24 @@ vim.keymap.set("n", "<leader>od", function()
         vim.cmd("diffthis")
     end
 end)
+
+-- Easier Moving between splits
+vim.keymap.set("n", "<C-J>", "<C-W><C-J>", { noremap = true})
+vim.keymap.set("n", "<C-K>", "<C-W><C-K>", { noremap = true })
+vim.keymap.set("n", "<C-L>", "<C-W><C-L>", { noremap = true })
+vim.keymap.set("n", "<C-H>", "<C-W><C-H>", { noremap = true })
+
+-- Make windows to be basically the same size
+vim.keymap.set("n", "<leader>=", "<C-W>=", { noremap = true })
+
+-- Sizing window horizontally
+-- to the right
+vim.keymap.set("n", "<C-.>", "<C-W>5<", { noremap = true })
+-- to the left
+vim.keymap.set("n", "<C-,>", "<C-W>5>", { noremap = true })
+
+-- Sizing window vertically
+-- taller
+vim.keymap.set("n", "<A-.>", "<C-W>5+", { noremap = true })
+-- shorter
+vim.keymap.set("n", "<A-,>", "<C-W>5-", { noremap = true })
