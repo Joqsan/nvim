bab5d9c8c5854b1a7eea7f1c6d17c80ce6b4d631
Joqsan Azocar <joqsan.a@gmail.com>
2024-04-28 13:01:17 +0300

    refactor nvim-dap-python config

--- after/plugin/nvim_dap_python.lua
+++ after/plugin/nvim_dap_python.lua
@@ -1,4 +1,17 @@
 local dap_python = require("dap-python")
+local dap = require("dap")
 
-adapter_python_path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+local adapter_python_path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
 dap_python.setup(adapter_python_path)
+
+-- Hard-coded for now.
+-- TODO:Try venv-selector
+dap_python.resolve_python = function()
+    return '/usr/bin/python3.11'
+end
+
+-- Somehow this line doesn't get set:
+-- https://github.com/mfussenegger/nvim-dap-python/blob/master/lua/dap-python.lua#L211
+dap.adapters.debugpy = dap.adapters.python
+
+require('dap.ext.vscode').load_launchjs()
