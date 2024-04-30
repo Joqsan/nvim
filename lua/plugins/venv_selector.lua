e8b5725d25f7e41eb6d7b86c39ba63300034e168
Joqsan Azocar <joqsan.a@gmail.com>
2024-05-01 01:40:03 +0300

    make venv-selector work also with .venv directories

--- lua/plugins/venv_selector.lua
+++ lua/plugins/venv_selector.lua
@@ -3,10 +3,11 @@ return {
   dependencies = {
     'neovim/nvim-lspconfig',
     'nvim-telescope/telescope.nvim',
-    'mfussenegger/nvim-dap-python' 
+    'mfussenegger/nvim-dap-python'
   },
   opts = {
-    dap_enabled = true
+    dap_enabled = true,
+    name = {"venv", ".venv"}
   },
   event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
   keys = {
