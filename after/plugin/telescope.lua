a23800aa4010177f2c6cf0afef3676d95e469c89
Joqsan Azocar <joqsan.a@gmail.com>
2024-05-01 01:30:28 +0300

    add new keymaps to telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,7 +3,7 @@ vim.keymap.set('n', '<leader>tf', builtin.find_files, {desc = "[Telescope]: list
 vim.keymap.set('n', '<leader>tg', builtin.git_files, {desc = "[Telescope]: fuzzy search output of git ls-files"})
 vim.keymap.set('n', '<leader>ts', builtin.git_status, {desc = "[Telescope]: show current git status and diff preview"})
 vim.keymap.set('n', '<leader>tb', builtin.current_buffer_fuzzy_find, {desc = "[Telescope]: fuzzy search inside open buffer"})
--- lr: show all the places in the project where the word under the cursor is referenced
+vim.keymap.set('n', '<leader>gs', builtin.grep_string, {desc = "[Telescope]: Search string under cursor in cwd"})
 vim.keymap.set(
 	'n',
 	'<leader>tr',
@@ -18,3 +18,4 @@ vim.keymap.set("n", "<leader>td", function() builtin.diagnostics {bufnr = 0} end
 vim.keymap.set('n', '<leader>th', builtin.help_tags, {desc = "[Telescope]: list available help tags. Ope with <CR>"})
 vim.keymap.set('n', '<leader>to', builtin.lsp_outgoing_calls, {desc = "[Telescope]: list LSP outgoing calls for word under the cursor"})
 vim.keymap.set('n', '<leader>ti', builtin.lsp_incoming_calls, {desc = "[Telescope]: list LSP incoming calls for word under the cursor"})
+vim.keymap.set('n', '<leader>tc', builtin.lsp_document_symbols, {desc = "[Telescope]: list LSP document symbols in the current buffer"})
